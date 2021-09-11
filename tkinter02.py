37c522f22769520d483504613833755db5c639b2
KEM001 <90522749+KEM001@users.noreply.github.com>
2021-09-11 23:02:04 +0530

    Add files via upload

---
+++ tkinter02.py
@@ -0,0 +1,10 @@
+from tkinter import*
+
+splash_root= Tk()
+splash_root.title("oh yeah")
+splash_root.geometry("300x200+-1500+250")
+
+splash_label= Label(splash_root, text="omg", font=("Helvetica",18))
+splash_label.pack()
+
+splash_root.mainloop()
