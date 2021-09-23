8e8f74b0da710d6799ec44b082c2610b6eecdc39
SOULSTROMER <90318170+SOULSTROMER@users.noreply.github.com>
2021-09-24 00:53:33 +0530

    Update tkinter02.py

--- tkinter02.py
+++ tkinter02.py
@@ -8,3 +8,32 @@ splash_label= Label(splash_root, text="omg", font=("Helvetica",18))
 splash_label.pack()
 
 splash_root.mainloop()
+
+def RCGENERATOR(x):
+    x = x.lower()
+    rc = ""
+    for i in x:
+        if i.isalpha() == True:
+            rc += str(ord(i) - 80)
+            
+        elif i.isnumeric() == True:
+            rc += str(int(i) + 50)
+    return f"{int(rc):o}"
+
+def blockchain():
+    
+    x = ["X100Y","Y100X"]
+    megablock = {}
+    fristtrans = True
+
+    for i in range(len(x)):
+        if fristtrans == True:
+            rc = RCGENERATOR(x[i])
+            megablock[rc] = ["BEGINNING",x[i]]
+            prevrc = rc
+            fristtrans = False
+        else:
+            rc = RCGENERATOR(x[i])
+            megablock[str(int(prevrc) + int(rc))] = [prevrc,x[i]]
+            prevrc = str(int(prevrc) + int(rc)) 
+    print(megablock)
